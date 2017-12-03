0740c1f1fd3615ec16dd63d1e6b99a7773e37cb6
yay897804105 <897804105@qq.com>
2017-12-04 00:03:05 +0800

    null

--- src/test/java/com/yxj/wechat/WechatApplicationTests.java
+++ src/test/java/com/yxj/wechat/WechatApplicationTests.java
@@ -1,8 +1,11 @@
 package com.yxj.wechat;
 
 import com.yxj.wechat.controller.HelloWorldController;
+import com.yxj.wechat.mapper.UserMapper;
+import com.yxj.wechat.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,63 +16,23 @@ import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = MockServletContext.class)
-//@WebAppConfiguration
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class WechatApplicationTests {
 
-	private MockMvc mvc;
 
-	public void setUp() throws Exception{
-		mvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
-	}
+
+//	private UserMapper userMapper;
+
+//	@Autowired
+//	private UserService userServiceImpl;
 
 	@Test
 	public void contextLoads() throws Exception {
-/*		RequestBuilder request = null;  asds
-		// 1、get查一下user列表，应该为空
-		request = get("/users/");
-		mvc.perform(request)
-				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("[]")));
+		System.out.print("asdada---------------------------------");
 
-		// 2、post提交一个user
-		request = post("/users/")
-				.param("id", "1")
-				.param("name", "测试大师")
-				.param("age", "20");
-		mvc.perform(request)
-				.andExpect(content().string(equalTo("success")));
+//		userServiceImpl.insert("yxj","yxj");
 
-		// 3、get获取user列表，应该有刚才插入的数据
-		request = get("/users/");
-		mvc.perform(request)
-				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"测试大师\",\"age\":20}]")));
-
-		// 4、put修改id为1的user
-		request = put("/users/1")
-				.param("name", "测试终极大师")
-				.param("age", "30");
-		mvc.perform(request)
-				.andExpect(content().string(equalTo("success")));
-
-		// 5、get一个id为1的user
-		request = get("/users/1");
-		mvc.perform(request)
-				.andExpect(content().string(equalTo("{\"id\":1,\"name\":\"测试终极大师\",\"age\":30}")));
-
-		// 6、del删除id为1的user
-		request = delete("/users/1");
-		mvc.perform(request)
-				.andExpect(content().string(equalTo("success")));
-
-		// 7、get查一下user列表，应该为空
-		request = get("/users/");
-		mvc.perform(request)
-				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("[]")));*/
 	}
 
 }
