263d57690a0d05b969644ff0e7e0af0daa0d20d8
yay897804105 <897804105@qq.com>
2017-12-04 00:01:43 +0800

    null

--- src/main/java/com/yxj/wechat/controller/HelloWorldController.java
+++ src/main/java/com/yxj/wechat/controller/HelloWorldController.java
@@ -1,14 +1,20 @@
 package com.yxj.wechat.controller;
 
 
+import com.yxj.wechat.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @Autowired
+    private UserService userServiceImpl;
+
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World";
+        userServiceImpl.insert("yxj","yxj");
+        return "Hello World YXJ";
     }
 }
