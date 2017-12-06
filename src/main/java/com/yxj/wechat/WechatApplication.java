ab7325c35688a43dc392bd08041c3880cdb822a9
yay897804105 <897804105@qq.com>
2017-12-07 00:04:35 +0800

    ...

--- src/main/java/com/yxj/wechat/WechatApplication.java
+++ src/main/java/com/yxj/wechat/WechatApplication.java
@@ -1,9 +1,11 @@
 package com.yxj.wechat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yxj.wechat.mapper")
 public class WechatApplication {
 
 	public static void main(String[] args) {
