6688f847c3a61e6bf189e63cb7604ab951725938
yay897804105 <897804105@qq.com>
2017-12-07 00:04:42 +0800

    ...

--- src/test/java/com/yxj/wechat/WechatApplicationTests.java
+++ src/test/java/com/yxj/wechat/WechatApplicationTests.java
@@ -1,12 +1,14 @@
 package com.yxj.wechat;
 
 import com.yxj.wechat.controller.HelloWorldController;
+import com.yxj.wechat.entity.User;
 import com.yxj.wechat.mapper.UserMapper;
 import com.yxj.wechat.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,11 +16,16 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WechatApplicationTests {
+
+
 
 
 
@@ -27,10 +34,22 @@ public class WechatApplicationTests {
 //	@Autowired
 //	private UserService userServiceImpl;
 
+//	private MockMvc mvc;
+//
+//	public void setUp() {
+//		mvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+//	}
+
+	@Autowired
+	private UserMapper userMapper;
 	@Test
 	public void contextLoads() throws Exception {
-		System.out.print("asdada---------------------------------");
-
+//		userMapper.insert("aa","cc");
+		List<User> userList = userMapper.getAllUsers();
+		System.out.println(userList.size()+"-----------------------------");
+		System.out.println("asdada---------------------------------");
+//		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+//			.andExpect()
 //		userServiceImpl.insert("yxj","yxj");
 
 	}
